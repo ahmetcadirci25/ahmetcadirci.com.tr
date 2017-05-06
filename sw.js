3865a442706492ce3da0cc4ea44e5b4065dabe10
Ahmet CADIRCI <ahmetcadirci25@gmail.com>
2017-05-06 23:05:12 +0300

    düzeltme

--- sw.js
+++ sw.js
@@ -25,8 +25,7 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('airhorner').then(function(cache) {
      return cache.addAll([
-       '/',
-       '/index.html',
+       '',
        '/css/main.css',
        '/js/main.js'
      ]);
