6766cc96710ed859d0063963a0e43e65559bbfc9
Ahmet Çadırcı <ahmetcadirci25@gmail.com>
2017-09-17 13:44:05 +0300

    düzenelem

--- sw.js
+++ sw.js
@@ -16,13 +16,6 @@ var urlsToCache = [
         
     {% endfor %}
 
-    {% for post in site.posts %}
-        '{{ post.url }}',
-    {% endfor %}
-
-    {% for file in site.static_files %}
-        '{{ file.path }}',
-    {% endfor %}
 ];
 self.addEventListener('install', function(event) {
   event.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
