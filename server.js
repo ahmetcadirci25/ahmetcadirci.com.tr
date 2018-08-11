152d3db383d5af93fc31bf4fe52db74191228338
Ahmet Çadırcı <ahmetcadirci25@gmail.com>
2018-08-11 23:29:59 +0300

    pwa

---
+++ server.js
@@ -0,0 +1,13 @@
+---
+layout: null
+---
+
+APP.get('/images/favicons/manifest.json', function(req, res) {
+    res.append('Content-Type', 'text/javascript');
+    res.sendFile(path.join(root, "manifest.json"));
+});
+
+APP.get('/sw.js', function(req, res) {
+    res.append('Content-Type', 'text/javascript');
+    res.sendFile(path.join(root, "sw.js"));
+});
